998cbbd23e31124b67ed7cadbefbdf9e18b8b5df
ingrex <thekingchinex123@gmail.com>
2025-08-30 16:07:19 +0100

    Initial React project with UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,22 @@
-import WelcomeMessage from './components/WelcomeMessage';
+// src/App.jsx
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
