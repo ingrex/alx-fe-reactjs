33289b90694a877fdbf687184b8072c8ebb3e9e6
ingrex <thekingchinex123@gmail.com>
2025-09-11 15:56:35 +0100

    recipe app design

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { useRecipeStore } from "../store/recipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <Link to={`/recipes/${recipe.id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
